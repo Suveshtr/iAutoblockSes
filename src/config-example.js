3b22f631ff705cfa56527d83ba438bb90bd45ea4
Suveshtr <suvesh.trivedi@gmail.com>
2018-02-14 17:49:39 +0530

    fix express to exports

--- src/config-example.js
+++ src/config-example.js
@@ -1,5 +1,5 @@
 exports.port = process.env.PORT || 5001
 exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`
-express.fromemail = process.env.FROMEMAIL || 'FROM_EMAIL'
-express.toemail = process.env.TOEMAIL || 'TO_EMAIL'
-express.allowedOrigin = process.env.ALLOWEDORIGIN || 'YOUR_DOMAIN'
+exports.fromemail = process.env.FROMEMAIL || 'FROM_EMAIL'
+exports.toemail = process.env.TOEMAIL || 'TO_EMAIL'
+exports.allowedOrigin = process.env.ALLOWEDORIGIN || 'YOUR_DOMAIN'
